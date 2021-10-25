6656a49769df5352b7a74e933673834e2f4ff238
KarlosEnd <karlos_end@hotmail.com>
2021-10-25 13:36:36 -0600

    player shader

--- Among Us/Assets/Scripts/Player/Sus.cs
+++ Among Us/Assets/Scripts/Player/Sus.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Sus : MonoBehaviour
 {
+    public GameObject body;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,18 @@ public class Sus : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                collision.gameObject.GetComponent<Animator>().Play("Dead");
+            }
+        }
         
     }
 }
